75f900215fafce910c934905ba30ef82b4a6f55d
walied elnour <wad3935@yahoo.com>
2022-01-03 16:04:50 +0300

    add spring API Gateway

---
+++ src/main/java/com/afro/SpringBootApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.afro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+
+
+public class SpringBootApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiGatewayApplication.class, args);
+	}
+
+}
